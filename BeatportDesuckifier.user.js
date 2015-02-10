01d22d234e4cd5d8c83046af8c33e91668b54aad
Joel Aaron Marshall <joelmdev2@gmail.com>
2015-02-10 13:07:53 -0500

    Updated to work with Beatport Pro

--- BeatportDesuckifier.user.js
+++ BeatportDesuckifier.user.js
@@ -1,25 +1,57 @@
-﻿// ==UserScript==
-// @name                Beatport Desuckifier
-// @namespace	        lowfrequencyresearch
-// @description	        Adds "queue release" link to my track beatport results
-// @require       http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js
-// @include		http://www.beatport.com/my-beatport*
+// ==UserScript==
+// @name        Beatport Desuckifier Pro
+// @namespace   lowfrequencyresearch
+// @description	        Adds a "queue all tracks from all releases" button to the top of My Beatport
+// @require       https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js
+// @include     https://pro.beatport.com/my-beatport*
+// @grant       none
 // ==/UserScript==
-
-
 $.noConflict();
-jQuery(document).ready(function ($)
-{
-    $("table.track-grid .colPlay").width('126px');
-    $(".track-grid-content.altRow-0.playRow.autoscroll, .track-grid-content.altRow-1.playRow.autoscroll").each(function ()
-    {
-        $(".tile-image-wrapper > a").length
-        var href = $(this).find(".tile-box.border-thin > .tile-image-wrapper > a").attr("href");
-        var splits = href.split("/");
-        var release = splits[splits.length - 1];
-        //deprecated with changes to beatport website March 2012
-        // var queueReleaseAnchor = '<a href="http://www.beatport.com' + href + '" data-play="release:' + release + '" class="btn-queue evtQueue jsOnly">queue release</a>';
-        var queueReleaseAnchor = '<span class="play-queue"><span class="btn-queue" data-item-id="' + release + '" data-item-type="release" data-player-action="queue" style="height:22px;width:50px;border-radius:20px;line-height:.9;color:white;font-size:11px">queue<br/>release</span></span>';
-        var html = $(this).find(".playColumn").html($(this).find(".playColumn").html() + queueReleaseAnchor);
+(function($) {
+    console.log("getting started!");
+    window.traxx;
+    while (!window.traxx) {
+        window.traxx = window.Playables.tracks;
+    }
+    console.log("got them traxx!");
+    window.traxx.attributes = [];
+    var lastReleaseId;
+    window.releaseTracks = "data-tracks='";
+    window.traxx.forEach(function(track) {
+        if (lastReleaseId != track.release.id) {
+            lastReleaseId = track.release.id;
+            console.log(lastReleaseId);
+            $.ajax({
+                type: 'GET',
+                url: "/api/releases/" + lastReleaseId + "/tracks",
+                dataType: 'json',
+                success: function(release) {
+                    release.tracks.forEach(function(track) {
+                        console.log(track.id);
+                        window.releaseTracks += (track.id + ",");
+                        window.traxx.attributes[track.id] = track;
+                    });
+                },
+                error: function(data) { alert("Fail!   " + JSON.stringify(data)); },
+                data: {},
+                async: false
+            });
+        }
     });
-});
+    window.releaseTracks += "'";
+    console.log(window.releaseTracks);
+    
+        var interval = setInterval(function() {
+            console.log("not yet");
+            if (typeof window.Beatport !== 'undefined' &&
+                typeof window.Beatport.Player !== 'undefined' &&
+                typeof window.Beatport.Player.playableRegistry !== 'undefined' &&
+                typeof window.Beatport.Player.playableRegistry.attributes !== 'undefined') {
+                console.log("in thar!");
+                window.Beatport.Player.playableRegistry.attributes = window.traxx.attributes;
+                clearInterval(interval);
+            }
+        }, 3000);
+    console.log("wrapping up!");
+    $(".bucket.tracks.standard-interior-tracks").prepend('<a class="icon icon-add-queue playable-queue-all" ' + window.releaseTracks + ' href="#">add all release tracks</a>');
+})(jQuery);
